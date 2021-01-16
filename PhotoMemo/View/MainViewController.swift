f803a4baa31e48f9fd7817173836541bf3baee50
nrurnru <nrurnru@gmail.com>
2021-01-17 02:31:23 +0900

    feat: CollectionView 기본 구성

--- PhotoMemo/View/MainViewController.swift
+++ PhotoMemo/View/MainViewController.swift
@@ -8,15 +8,37 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    var data = [1,2,3,4,5]
+    @IBOutlet var memoCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupUI()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.navigationItem.title = "메모 목록"
     }
+    func setupUI() {
+        memoCollectionView.delegate = self
+        memoCollectionView.dataSource = self
+        
+        let nib = UINib(nibName: "MemoCollectionViewCell", bundle: nil)
+        memoCollectionView.register(nib, forCellWithReuseIdentifier: "memoCell")
+    }
 }
 
+extension MainViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return data.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "memoCell", for: indexPath) as! MemoCollectionViewCell
+        cell.text?.text = indexPath.row.description
+
+        return cell
+    }
+}
---
+++ PhotoMemo/View/MemoCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  MemoCollectionViewCell.swift
+//  PhotoMemo
+//
+//  Created by 최광현 on 2021/01/17.
+//
+
+import UIKit
+
+class MemoCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet var text: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+    }
+
+}
