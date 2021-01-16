fbbbb90b9daace56418bb6711d8be6f72f9d0d5e
nrurnru <nrurnru@gmail.com>
2021-01-16 23:52:48 +0900

    Initial Commit

---
+++ PhotoMemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoMemo
+//
+//  Created by 최광현 on 2021/01/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
